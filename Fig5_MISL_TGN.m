4faeedea3bcb81ec812c3b12aa99d3d2a6efd033
Jianpan Huang <43700029+JianpanHuang@users.noreply.github.com>
2023-09-26 11:57:34 +0800

    Update Fig5_MISL_TGN.m

--- Fig5_MISL_TGN.m
+++ Fig5_MISL_TGN.m
@@ -21,7 +21,7 @@ img_all = [img_base,img_28min,img_60min];
 t = -4:4:60; % in min
 figure,set(gcf,'unit','normalized','position',[0.15,0.4,0.8,0.2],'color','w');
 subplot(1,2,1),plot(t, dynamic,'-o',LineWidth=2,MarkerSize=8); set(gca,'fontsize',18);
-xlabel("t (min)"); ylabel("MT"); xlim([min(t),max(t)]); ylim([0.1,0.3]);
+xlabel("t (min)"); ylabel("MISL"); xlim([min(t),max(t)]); ylim([0.1,0.3]);
 subplot(1,2,2),imagesc(img_all); colorbar; caxis([0,0.3]); mycolormap(1);axis off; set(gca,'fontsize',18);
 
 
